42c2851004d0d67a9b043490510c48292b48e764
Murli Jha <murlijh@gmail.com>
2025-10-01 17:52:16 +0530

    Initial Skelton (#1)

--- README.md
+++
@@ -1,2 +0,0 @@
-# m2j-code-lab
-is a personal project for learning, practicing, and experimenting with Data Structures &amp; Algorithms (DSA)
---
+++ src/main/java/com/m2j/codelab/M2jCodeLabApplication.java
@@ -0,0 +1,13 @@
+package com.m2j.codelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2jCodeLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2jCodeLabApplication.class, args);
+	}
+
+}
